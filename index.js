40682f609c6deaeb6def47d796cc3b99f5ef0c4f
Tenzin Jorden <tenden96@gmail.com>
2021-12-16 10:44:22 -0800

    commit tree 5048a83fbfc016c300693f78ea0d654d2d24e090

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+
+function findMatching(drivers,name){
+        return drivers.filter(function(driver){ return driver.toLowerCase() === name.toLowerCase()})
+}
+function fuzzyMatch(drivers,name){
+    return drivers.filter(function(driver){return driver.toLowerCase().substring(0,name.length)===name.toLowerCase()})
+}
+
+function matchName(drivers,name) {
+    return drivers.filter(function(driver){return driver.name === name})
+
+}
+
+
